402263d2744c7d0071150ae48761fd301d3bdc6e
Poilon <poilon@gmail.com>
2020-03-26 14:35:11 +0100

    first commit !

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
