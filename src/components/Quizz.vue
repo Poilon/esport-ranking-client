1588766c289005bbbce50b07104832552d7fa4a1
Yannis PERROT <perrot-yannis@orange.fr>
2020-05-12 02:50:16 +0200

    shuffle answers, random tourney, prettier

--- src/components/Quizz.vue
+++ src/components/Quizz.vue
@@ -1,16 +1,11 @@
 <template>
-    <v-container style="max-width:1024px; padding:24px;">
-
-        <!-- HEADER - NAME + SCORE (TO BE REDESIGNED) -->
-        <v-toolbar color="transparent" flat class="mt-2 ml-0">
-            Hello, {{playerName}}! Your current score is {{currentScore}}.
-        </v-toolbar>
+    <v-container style="max-width:1024px; padding:24px;">   
 
         <div class="text-center">
 
             <!-- Quizz Starting - Temporal modal to enter your name
             Once name is entered, the game starts -->
-            <v-btn rounded dark v-if="!launched" @click="dialog = true">
+            <v-btn rounded dark v-if="!launched && !loading" @click="dialog = true">
                 Start a quizz
             </v-btn>
             <v-dialog v-model="dialog" persistent width="500">
@@ -39,28 +34,53 @@
                 </v-card>
             </v-dialog>
 
-            <!-- Game itself
-            Currently only "WHO WON THE TOURNAMENT X" -->
-            <div v-if="launched">
-                <div>
-                    {{currentQuestion}}
-                </div>
+            <!-- CORE GAME  -->
+            <v-card
+                v-if="launched"
+                class="mx-auto"
+                outlined
+            >
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="headline mb-1">{{playerName}}: {{currentScore}} points</v-list-item-title>
+                        <v-list-item-subtitle>{{currentQuestion}}</v-list-item-subtitle>
+                        <v-card-text>
+                            <v-row>
+                                    <v-col 
+                                        v-for="n in MAX_ANSWERS" 
+                                        outlined
+                                        :key="n"
+                                        cols="6"
+                                        @click="checkAnswer(answersToDisplay[n-1])">
+                                        <v-hover
+                                            v-slot:default="{ hover }"
+                                        >
+                                            <v-card 
+                                                :elevation="hover ? 12 : 2"
+                                                class="pa-2"
+                                                v-ripple="{ center: true }"
+                                                outlined
+                                                tile
+                                            >
+                                                {{answersToDisplay[n-1]}}
+                                            </v-card>
+                                        </v-hover>
+                                    </v-col>
+                                    <!--
+                                    <v-responsive
+                                    v-if="n === 2"
+                                    :key="`width-${n}`"
+                                    width="100%"
+                                    ></v-responsive> -->
+                                </v-row>
+                            {{TIMER}} <span v-if="TIMER <= 1">second</span><span v-if="TIMER > 1">seconds</span> left...
+                        </v-card-text>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-card>
+        </div>
 
-                <v-col 
-                    v-for="n in MAX_ANSWERS"
-                    :key="n"
-                >
-                    <div class="pa-2">
-                        <v-btn @click="checkAnswer(answersToDisplay[n-1])">
-                            {{answersToDisplay[n-1]}}
-                        </v-btn>
-                    </div>
-                </v-col>
-
-                <div>
-                    {{TIMER}}
-                </div>
-            </div>
+            <!-- DIALOGS -->
 
             <!-- Won or Lost -->
             <v-dialog v-model="dialogResult" persistent width="500">
@@ -121,8 +141,6 @@ export default {
         currentQuestion: "",
         currentScore: 0,
         currentTournament: {},
-        currentTournamentIndex: "",
-        tournaments: [],
         results: [],
         dialog: false,
         dialogResult: false,
@@ -134,43 +152,31 @@ export default {
         answers: new Map(),
         result: "",
         MAX_ANSWERS: 4,
+        MAX_TOURNAMENTS: 10,
         TIMER: '',
         interval: '',
-        ids: [
-            "eba12023-9bde-47a0-9056-d7e159dc636d",
-            "83cb8ca0-3f30-4c9c-905f-72722e26b6f7",
-            "ed4e95be-7faf-4e0f-bfd4-27c4127dc569",
-            "a5fccccc-7e5c-4c5b-940f-a30932053686",
-            "7f7ba47b-f85d-4a5e-81c2-7b2aafd94ecd",
-        ],
     }),
     mounted() {
-        this.generateTournaments();
+        this.generateTournament();
     },
     methods: {
         // Query to look for tournaments data
-        // Currently only one tournament because idk how IDs work in SmashGG
-        generateTournaments() {
-            this.ids.forEach((id) => {
-                var idTmp = id;
-                this.$apollo
-                    .query({
-                        query: gql`
-                            query FetchTournament($id: String!){
-                            tournament (id: $id) {
-                                name
-                                results { rank player { id name score profile_picture_url }}
-                            }
-                        }`,
-                        variables: {
-                            id: idTmp
+        generateTournament() {
+            this.loading = true
+            this.$apollo
+                .query({
+                    query: gql`{
+                        random_tournament {
+                            name
+                            results { rank player { name profile_picture_url }}
                         }
-                    })
-                    .then(data => {
-                        this.tournaments.push(data.data.tournament);
-                        this.loading = false
-                    });
-            })
+                    }`
+                })
+                .then(data => {
+                    this.currentTournament = data.data.random_tournament;
+                    console.log(this.currentTournament.name)
+                    this.loading = false
+                });
         },
         // Launching the game (currently )
         // "answers" is a map containing 1 correct answer & 3 wrong answers
@@ -180,17 +186,26 @@ export default {
         quizz() {
             this.launched = true;
             this.dialog = false;
-            this.generateQuestion(this.currentTournament);
+            this.generateQuestion();
             for (var i = 0 ; i < 4 ; i++) {
                 this.answersToDisplay[i] = this.currentTournament.results[i].player.name;
                 this.answers.set(this.currentTournament.results[i].player.name, i === 0 ? true : false);
             }
+            this.shuffleAnswers()
             this.TIMER = 10
 
             this.interval = setInterval(this.countdown, 1000)
         },
-        generateQuestion(tournament) {
-            this.currentQuestion = "Who won " + tournament.name + "?";
+        generateQuestion() {
+            this.currentQuestion = "Who won " + this.currentTournament.name + "?";
+        },
+        shuffleAnswers() {
+            for (var l = this.answersToDisplay.length - 1 ; l >= 0 ; l--) {
+                var index = Math.floor(Math.random() * l)
+                var tmp = this.answersToDisplay[l]
+                this.answersToDisplay[l] = this.answersToDisplay[index]
+                this.answersToDisplay[index] = tmp
+            }
         },
         checkAnswer(answer){
             clearTimeout(this.interval)
@@ -204,6 +219,7 @@ export default {
                 this.result = "Wrong answer... You earned no point."
             }
             this.dialogResult = true;
+            this.generateTournament();
         },
         countdown() {
             // timer runs out, answer is false = lost
@@ -221,12 +237,11 @@ export default {
                 this.currentTournamentIndex = 0
             } else {
                 this.currentTournamentIndex++
-                if (this.currentTournamentIndex == this.tournaments.length) {
+                if (this.currentTournamentIndex == this.MAX_TOURNAMENTS) {
                     this.endGame(true)
                     return;
                 }
             }
-            this.currentTournament = this.tournaments[this.currentTournamentIndex]
             this.quizz()
         },
         endGame(end) {
