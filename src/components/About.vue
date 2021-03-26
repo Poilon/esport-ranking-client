a1a10636439a7c00c2adc47fa1e5e8dfc171a53c
Ryuker <74071324+Ryuker@users.noreply.github.com>
2021-03-26 17:27:41 +0100

    Update About.vue
    Added links to BTS stream for Phoenix Blue Online and Regen Online,

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,11 +6,17 @@
       <p>Times have changed but we’re Still. Arranging. Melee. Events. (or S.A.M.E for short).</p>
 
       <p>A collective of 11 premier European TO teams have joined forces under the S.A.M.E Circuit to give you a taste of some the best that European Melee can still offer (under the current circumstances).</p>
-
-      <b><p>Registration is live for the next S.A.M.E events Phoenix Blue Online!
+      
+      <b><p>Tune in all weekend to the third S.A.M.E major, Phoenix Blue Online on</b> </p>
+      <p> <a target="_blank" href="https://www.twitch.tv/btssmash">twitch.tv/btssmash> & <a target="blank" href="https://www.twitch.tv/btssmash2">twitch.tv/btssmash2</a>! 
       </p>
 
-      <p>Register at <a href="https://smash.gg/pxbo" target="_blank">smash.gg/pxbo</a></p></b>
+      <b><p>Registration is live for the next S.A.M.E event Regen Online!</b></p> 
+      <br>The UK's first online Smash major & charity event in 2021 hosted on <b>April 23rd-26th</b>!Consider to donate to their <a target="_blank" href="https://www.specialeffect.org.uk/">SpecialEffect</a> Fundraiser at the link below!
+<a target="blank" href="https://www.justgiving.com/RegenOnline">justgiving.com/RegenOnline</a>
+      </p>
+
+      <p>Register at <a target="_blank" href="https://smash.gg/regen" target="_blank">smash.gg/regen</a></p></b>
 
       <p>More information will be added to this website soon but for now this website only houses the leaderboards.
       For more information about the circuit see <a href="https://smash.gg/SAMEcircuit" target="_blank">smash.gg/SAMEcircuit</a></p>
