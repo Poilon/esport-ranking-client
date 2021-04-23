a6487f0cb37888e0db151f060be3ccbc572a2743
Ryuker <74071324+Ryuker@users.noreply.github.com>
2021-04-23 02:53:38 +0200

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -19,7 +19,7 @@
       <p>More information will be added to this website soon but for now this website only houses the leaderboards.
       For more information about the circuit see <a href="https://smash.gg/SAMEcircuit" target="_blank">smash.gg/SAMEcircuit</a></p>
 
-      <p>We're proud to reveal S.A.M.E 2021 is also a qualifier for <b>Smash Summit 11</b>. The 1st place on the singles leaderboards after Fête concludes earns an invite to the acclaimed invitational by <a target="_blank" href="https://beyondthesummit.tv/>"Beyond The Summit</a></p>
+      <p>We're proud to reveal S.A.M.E 2021 is also a qualifier for <b>Smash Summit 11</b>. The 1st place on the singles leaderboards after Fête concludes earns an invite to the acclaimed invitational by <a target="_blank" href="https://beyondthesummit.tv/>Beyond The Summit</a></p>
 
       <p>When registering for an event please make sure to read the <b><a target="blank" href="https://docs.google.com/document/d/1vlzR5-27qDeAmpWxqbQb7133jSQ-7gaZr3wTz5fR_Vg/edit?usp=sharing">S.A.M.E ruleset document</a></b></p>
 
