1621648917a4acf64558958a92f3638dca3c4e07
Poilon <poilon@gmail.com>
2021-04-26 23:04:25 +0200

    about

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,15 +6,25 @@
 
       <p>A collective of 11 premier European TO teams have joined forces under the S.A.M.E Circuit to give you a taste of some the best that European Melee can still offer (under the current circumstances).</p>
 
-      <p>Tune in from Friday till Sunday to our 4th major, <b><a target="_blank" href="https://smash.gg/tournament/regen-online/details">Regen Online</a><b>, on <b><a target="_blank" href="https://twitch.tv/btssmash">BTSsmash</a></b> and <b><a target="_blank" href="https://twitch.tv/btssmash">BTSsmash 2</a></b></p>
+      <p>Tune in from Friday till Sunday to our 4th major,
+        <b>
+          <a target="_blank" href="https://smash.gg/tournament/regen-online/details">Regen Online</a>
+        </b>
+        , on
+        <b>
+          <a target="_blank" href="https://twitch.tv/btssmash">BTSsmash</a>
+        </b> and <b>
+          <a target="_blank" href="https://twitch.tv/btssmash">BTSsmash 2</a>
+        </b>
+      </p>
 
       <p>Regen Online is raising funds for <a target="_blank" href="https://www.specialeffect.org.uk/">SpecialEffect</a> a charity driven to put fun and inclusion back into the lives of people with physical disabilities by helping them to play video games.
       <p>Consider to donate at <a target="blank" href="https://www.justgiving.com/RegenOnline">justgiving.com/RegenOnline</a></p>
 
-      <b><p>Registration is live for the next S.A.M.E event, Smash Sauna Online!</b></p>
+      <p><b>Registration is live for the next S.A.M.E event, Smash Sauna Online!</b></p>
       <p>SmashFinland will be heating up the competition on <b>21-23</b> of May with Melee Singles, Teams, Ladder and Smash Sisters and R5 of international crews!</p>
 
-      <p><b>Sweat of the stress and register at</b> <a target="_blank" href="https://smash.gg/smashsauna" target="_blank">smash.gg/smashsauna</a></b></p>
+      <p><b>Sweat of the stress and register at</b> <a href="https://smash.gg/smashsauna" target="_blank">smash.gg/smashsauna</a></b></p>
 
       <p>More information will be added to this website soon but for now this website only houses the leaderboards.
       For more information about the circuit see <a href="https://smash.gg/SAMEcircuit" target="_blank">smash.gg/SAMEcircuit</a></p>
