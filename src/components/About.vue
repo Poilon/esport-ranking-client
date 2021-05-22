054fb995d072f863e72feca06ae23ad7528f8ea5
Ryuker <74071324+Ryuker@users.noreply.github.com>
2021-05-22 21:17:46 +0200

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,14 +5,14 @@
       <p>Times have changed but we’re <b>S</b>till. <b>A</b>rranging. <b>M</b>elee. <b>E</b>vents. (or S.A.M.E for short).</p>
 
       <p>A collective of 11 premier European TO teams have joined forces under the S.A.M.E Circuit to give you a taste of some the best that European Melee can still offer (under the current circumstances).</p>
+      
+      <p><b>Tune in this weekend to 🔥Smash Sauna Online🔥! on <a target="_blank" href="https://www.twitch.tv/btssmash/squad>btssmash/squad</a></b></p> and help raise funds for Finnish children's hospitals
+      <p>SmashFinland will is heating up the competition with Melee Singles, Teams, Ladder and Smash Sisters and R5 of international crews!</p>
 
-      <p><b>Registration is live for the next S.A.M.E event, Smash Sauna Online!</b></p>
-      <p>SmashFinland will be heating up the competition on <b>21-23</b> of May with Melee Singles, Teams, Ladder and Smash Sisters and R5 of international crews!</p>
+      <p><b>Registration is live for the next S.A.M.E event, Fête Online!</b></p>
+      <p>TeamFête is hosting a week long party in their Minecraft venue from the <b>21st-27th of June</b> with Melee Singles, Teams, R5 of international crews, an Iron man bracket and much more to be revealed!</p>
 
-      <p><b>Sweat of the stress and register at</b> <a href="https://smash.gg/smashsauna" target="_blank">smash.gg/smashsauna</a></b></p>
-
-      <p>Smash Sauna Online is raising funds for <a target="_blank" href="https://kummit.fi/tietoa-meista/kummit-ry-info/in-english/">Kummit</a> a charitable organization, which supports the five Finnish university children’s hospitals, development and research, and attempts to improve the comfort of patients during their hospital stay. The association has also placed significant attention on the mental health work of children and adolescents.
-      <p>Consider to donate at <a target="blank" href="https://oma.kummit.fi/yleinen/16-3957">oma.kummit.fi/yleinen/16-3957</a></p>
+      <p><b>Register at</b> <a href="https://smash.gg/fete" target="_blank">smash.gg/fete</a></b>🥳</p>
 
       <p>More information will be added to this website soon but for now this website only houses the leaderboards.
       For more information about the circuit see <a href="https://smash.gg/SAMEcircuit" target="_blank">smash.gg/SAMEcircuit</a></p>
