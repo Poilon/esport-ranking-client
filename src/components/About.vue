2cc45274190137653e926f89e00b48a433251107
Ryuker <74071324+Ryuker@users.noreply.github.com>
2021-09-24 16:41:13 +0200

    Update About.vue
    Updated for Bagarre weekend

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,16 +6,13 @@
 
       <p>A collective of 11 premier European TO teams have joined forces under the S.A.M.E Circuit to give you a taste of some the best that European Melee can still offer (under the current circumstances).</p>
       
-      <p><b>Registration is live for the 9th and final S.A.M.E qualifier, Bagarre!</b>
-      <br>Join <a target="_blank" href="https://twitter.com/ParisSmashGang">ParisSmashGang</a> for an online scuffle on the <b>25th -26th of September</b> with Melee Singles, Teams, Round 9 of International Crews and a combo contest!</p>
-
-      <p><b>Register at </b><a href="https://smash.gg/bagarre" target="_blank">smash.gg/bagarre</a></b></p>
-
+      <p><b>Tune in all weekend to the 9th and final S.A.M.E qualifier,<a target="_blank" href="https://smash.gg/bagarre"> Bagarre</a> with Melee Singles, Teams, Round 9 of International Crews and a combo contest! Streamed on <a target="_blank" href="https://twitch.tv/btssmash/squad">BTS Smash</a></b>.
+ 
       <p>More information will be added to this website soon but for now this website only houses the leaderboards.
       <br>For more information about the circuit see <a href="https://smash.gg/SAMEcircuit" target="_blank">smash.gg/SAMEcircuit</a></p>
 
       <p>We're proud that S.A.M.E 2021 is also a qualifier for <b>Smash Summit 11</b> and <b>Smash Summit 12</b>. After unfortunate travel issues, we wish <a target="_blank" href="https://samecircuit.eu/#/players/d598526e-2531-47d7-84ec-d7bb46eea3cd">Pipsqueak</a> the best of luck at Smash Summit 12 by <a target="_blank" href="https://beyondthesummit.tv/">Beyond The Summit</a>
-      <br>The highest placing uninvited singles player at the <b>SAME Finale</b> will also earn an invite to <b>Smash Summit 12</b> in December! Top 16 on the singles leaderboards qualify for the Finale!</p>
+      <br>The singles champion of the <b>SAME Finale</b> will also earn an invite to <b>Smash Summit 13</b> in Summer 2022! Top 16 on the singles leaderboards qualify for the Finale!</p>
       
       <p>When registering for an event please make sure to read the <b><a target="blank" href="https://docs.google.com/document/d/1vlzR5-27qDeAmpWxqbQb7133jSQ-7gaZr3wTz5fR_Vg/edit?usp=sharing">S.A.M.E ruleset document</a></b></p>
 
