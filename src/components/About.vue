80f1e4b22f96e5372e6bee2bd65c29f24795deff
Ryuker <74071324+Ryuker@users.noreply.github.com>
2021-09-27 19:40:00 +0200

    Update About.vue
    updated after Bagarre with Finale text

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,12 +6,18 @@
 
       <p>A collective of 11 premier European TO teams have joined forces under the S.A.M.E Circuit to give you a taste of some the best that European Melee can still offer (under the current circumstances).</p>
       
-      <p><b>Tune in all weekend to the 9th and final S.A.M.E qualifier,<a target="_blank" href="https://smash.gg/bagarre"> Bagarre</a> with Melee Singles, Teams, Round 9 of International Crews and a combo contest! Streamed on <a target="_blank" href="https://twitch.tv/btssmash/squad">BTS Smash</a></b>.
+      <p>After 9 events, each ran by a unique premier European TO team, the top 16 singles competitors, the top 8 doubles teams and the top 8 international crews will battle it out at the <b>S.A.M.E FINALE</b> for the champion titles!</p>
+
+      <p>Tune in on November 5th to 7th to BTS Smash for the conclusion of the S.A.M.E 2021 circuit..!<p>
+
+      <p>There's an invite for <b>Smash Summit 13</b> (to be held in the Summer of 2022) on the line for the Circuit singles champion. The Doubles champions will earn an invite to <b>Heir 6</b>, provided by <a target="_blank" href="https://twitter.com/teamheir">Team Heir</a>.<p>
+
+      <p>And we'll find out once and for all which country crew comes out on top as our <b>International Crews Champion</b> after a year long round robin concluding in a single elimination bracket at the FINALE.
  
       <p>More information will be added to this website soon but for now this website only houses the leaderboards.
       <br>For more information about the circuit see <a href="https://smash.gg/SAMEcircuit" target="_blank">smash.gg/SAMEcircuit</a></p>
 
-      <p>We're proud that S.A.M.E 2021 is also a qualifier for <b>Smash Summit 11</b> and <b>Smash Summit 12</b>. After unfortunate travel issues, we wish <a target="_blank" href="https://samecircuit.eu/#/players/d598526e-2531-47d7-84ec-d7bb46eea3cd">Pipsqueak</a> the best of luck at Smash Summit 12 by <a target="_blank" href="https://beyondthesummit.tv/">Beyond The Summit</a>
+      <p>We're proud that S.A.M.E 2021 is also a qualifier for <b>Smash Summit 11</b> and <b>Smash Summit 13</b>. After unfortunate travel issues, we wish <a target="_blank" href="https://samecircuit.eu/#/players/d598526e-2531-47d7-84ec-d7bb46eea3cd">Pipsqueak</a> the best of luck at Smash Summit 12 by <a target="_blank" href="https://beyondthesummit.tv/">Beyond The Summit</a>
       <br>The singles champion of the <b>SAME Finale</b> will also earn an invite to <b>Smash Summit 13</b> to be held in the Summer of 2022! Top 16 on the singles leaderboards qualify for the Finale!</p>
       
       <p>When registering for an event please make sure to read the <b><a target="blank" href="https://docs.google.com/document/d/1vlzR5-27qDeAmpWxqbQb7133jSQ-7gaZr3wTz5fR_Vg/edit?usp=sharing">S.A.M.E ruleset document</a></b></p>
