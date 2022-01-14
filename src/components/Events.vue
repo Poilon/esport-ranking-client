bf2317d28764ae00b31bec77221715642a92320c
Ryuker <74071324+Ryuker@users.noreply.github.com>
2022-01-14 14:17:47 +0100

    Update Events.vue
    changed finale-sticker spot to finale-sticker since circuit has concluded

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -82,7 +82,7 @@
         <v-col sm4 cols12>
           <a href="https://smash.gg/same-finale-2021" target="_blank">
             <v-img src="../assets/tournaments/10_SAMEcal_november.png" style="width: 100%; height: 270px; max-height: 270px; max-width: 300px; margin-left: auto; margin-right: auto;" />
-            <v-img src="../assets/tournaments/10_samecal_nov_sticker_spot.png" style="width: 100%; height: 250px; max-height: 270px; max-width: 300px; margin-left: auto; margin-right: auto; margin-top: -250px;" />
+            <v-img src="../assets/tournaments/10_SAMEcal_nov_finale_sticker.png" style="width: 100%; height: 250px; max-height: 270px; max-width: 300px; margin-left: auto; margin-right: auto; margin-top: -250px;" />
           </a>
         </v-col>
 
